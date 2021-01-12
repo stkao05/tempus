f94b734fabe070e010815f15a525b83845194844
Steven Kao <st.kao.05@gmail.com>
2021-01-12 15:42:48 +0800

    test page

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,57 @@
 (function () {
-  function setPage() {
-    let page = window.location.hash
-      ? window.location.hash.substring(1)
-      : "intro";
+  function initTestPage() {
+    function initRangeControl(rangeControl, value, onChange) {
+      const input = rangeControl.querySelector("input[type='range']");
+      const valueText = rangeControl.querySelector(".range-value");
 
-    document.documentElement.setAttribute("page", page);
+      input.value = value;
+
+      const displayValue = () => {
+        valueText.textContent = input.value;
+        onChange(input.value);
+      };
+
+      input.addEventListener("input", displayValue);
+      displayValue();
+    }
+
+    let paramFontSize = 32;
+    let paramLineHeight = 1.5;
+    const testText = document.getElementById("page-test-text");
+
+    function updateTestTextStyle() {
+      testText.style.fontSize = `${paramFontSize}px`;
+      testText.style.lineHeight = `${paramLineHeight}`;
+    }
+
+    const rangeFontSize = document.getElementById("range-fontsize");
+    const rangeLineHeight = document.getElementById("range-line-height");
+
+    initRangeControl(rangeFontSize, paramFontSize, (value) => {
+      paramFontSize = value;
+      updateTestTextStyle();
+    });
+    initRangeControl(rangeLineHeight, paramLineHeight, (value) => {
+      paramLineHeight = value;
+      updateTestTextStyle();
+    });
+
+    updateTestTextStyle();
   }
 
-  window.addEventListener("hashchange", setPage, false);
-  setPage();
+  function initPageControl() {
+    function setPage() {
+      let page = window.location.hash
+        ? window.location.hash.substring(1)
+        : "intro";
+
+      document.documentElement.setAttribute("page", page);
+    }
+
+    window.addEventListener("hashchange", setPage, false);
+    setPage();
+  }
+
+  initPageControl();
+  initTestPage();
 })();
