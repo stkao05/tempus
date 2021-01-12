e623405259f87ff27c12ca843073594d9b267c12
Steven Kao <st.kao.05@gmail.com>
2021-01-12 17:17:00 +0800

    image switch

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,27 @@
 import initTestPage from "./test_page";
 
 (function () {
-  function initPageControl() {
+  function initImageRotate() {
+    const images = document.querySelectorAll(".image-rotate img");
+
+    let index = 0;
+    function showImg() {
+      images.forEach((elm) => {
+        elm.style.display = null;
+      });
+      images[index].style.display = "block";
+    }
+
+    const container = document.querySelector(".image-rotate");
+    container.addEventListener("click", () => {
+      index = (index + 1) % images.length;
+      showImg();
+    });
+
+    showImg();
+  }
+
+  function initPageSwitch() {
     function setPage() {
       let page = window.location.hash
         ? window.location.hash.substring(1)
@@ -14,6 +34,7 @@ import initTestPage from "./test_page";
     setPage();
   }
 
-  initPageControl();
+  initPageSwitch();
   initTestPage();
+  initImageRotate();
 })();
